93dc16dc857427218821d1fbeecad16058a3dee3
Gustavo Habermann <gustavo.francisco.habermann@gmail.com>
2026-08-06 14:50:44 -0300

    Adding color to pieces in terminal

--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,11 @@
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
     public static void printBoard(ChessPiece[][] pieces){
         for(int i = 0; i < pieces.length; i++){
@@ -17,7 +22,11 @@ public class UI {
         if(piece == null){
             System.out.print("-");
         } else {
-            System.out.print(piece);
+            if(piece.getColor() == Color.WHITE){
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
         System.out.print(" ");
     }
