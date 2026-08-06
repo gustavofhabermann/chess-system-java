2f54877d83286a425bf244bc940fa9f58c6eab92
Gustavo Habermann <gustavo.francisco.habermann@gmail.com>
2026-08-06 13:15:18 -0300

    First Class: Position

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         System.out.println("chess system");
+
+        Position position = new Position(1, 1);
+        System.out.println(position);
     }
 }
