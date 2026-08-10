3dc72f8498825dec0efc976c207ee62f4eb546a8
Gustavo Habermann <gustavo.francisco.habermann@gmail.com>
2026-08-10 15:06:52 -0300

    Switching player each turn

--- src/App.java
+++ src/App.java
@@ -14,7 +14,7 @@ public class App {
         while(true){
             try {
                 UI.clearScreen();
-                UI.printBoard(cm.getPieces());
+                UI.printMatch(cm);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
